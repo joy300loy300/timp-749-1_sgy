246f3908878c190e92730fe3a4def46157cf20d3
joy300loy300 <joy300loy300@gmail.com>
2021-04-20 15:05:18 +0700

    add pr4, upd README.md

--- 01_lists.c
+++ 01_lists.c
@@ -1,177 +1,282 @@
 #include <stdio.h>
-#include <malloc.h>
-#include <stdbool.h>
+#include <stdlib.h>
+
 struct list
 {
-    struct list *next;
-    int data;
-};
+    struct node* f;
+    struct node* l;
+} list;
+
+
 struct node
-{
+{ 
+    int value;
     struct node *next;
-    int data;
-};
-void init(struct list **l)
+    struct node *prev;
+} node;
+
+
+void init(struct list* myList,int value) 
 {
-    *l = (struct list *)malloc(sizeof(struct list));
-    (*l)->next = NULL;
-    (*l)->data = 2147483647;
+    struct node* m;
+    m = (struct node*)malloc(sizeof(struct node));
+    m->value=value;
+    m->next=NULL;
+    m->prev=NULL;
+    myList->f = m;
+    myList->l = m;
 }
-void clean(struct list *l)
+
+
+int is_empty(struct list* myList) 
 {
-    struct list *Temp = l;
-    do
+    return myList->f==NULL && myList->l==NULL;
+}
+
+
+int clean(struct list* myList) 
+{
+    if(is_empty(myList)==1)
     {
-        Temp = Temp->next;
-        free(l);
-        l = Temp;
-        
-    }
-    while (l != NULL);
-}
-bool is_empty(struct list *l)
-{
-    if (l == NULL)
-    return 0;
-    else
-    return 1;
-}
-struct node *find(struct list *l, int value)
-{
-    struct list *Temp = l;
-    while (1)
-    {
-        if (Temp == NULL)
-        return NULL;
-        else
+        struct node *m=myList->f;
+        struct node *e=NULL;
+        while(m!=NULL)
         {
-            if ((Temp->data) == value)
-            return ((struct node *)l);
-            
+            e=m->next;
+            free(m);
+            m=e;
         }
-        Temp = Temp->next;
-        
+        myList->f=NULL;
+        myList->l=NULL;
     }
 }
-int push_back(struct list *l, int value)
+
+
+struct node* find(struct list* myList,int k)
 {
-    if (l->data == 2147483647)
-    {
-        l->data = value;
-        return 0;
-        
-    }
-    struct list *Temp = (struct list *)malloc(sizeof(struct list));
-    Temp = l;
-    while (Temp->next != NULL)
-    Temp = Temp->next;
-    Temp->next = (struct list *)malloc(sizeof(struct list));
-    Temp = Temp->next;
-    Temp->data = value;
-    Temp->next = NULL;
-    return 0;
-}
-int push_front(struct list **l, int value)
-{
-    struct list *Temp = (struct list *)malloc(sizeof(struct list));
-    Temp->data = value;
-    Temp->next = (*l);
-    (*l) = Temp;
-    return 0;
-}
-int insert_after(struct node *l, int value)
-{
-    struct list *Temp = (struct list *)malloc(sizeof(struct list)), *Temp2;
-    Temp2=(struct list *)l->next;
-    l->next =(struct node *)Temp;
-    Temp->data = value;
-    Temp->next = Temp2;
-    return 0;
-}
-int remove_node(struct list *l, int value)
-{
-    struct list *Temp = l;
-    if (l != NULL)
-    {
-        while (1)
-        {
-            if ((Temp->next) == NULL)
-            return 1;
-            if ((Temp->next->data) == value)
-            {
-                struct list *Temp2 = (struct list *)malloc(sizeof(struct list));
-                Temp2 = Temp->next->next;
-                free(Temp->next);
-                Temp->next = Temp2;
-                return 0;
-            }
-            Temp = Temp->next;
+    struct node *m=myList->f;
+    while(m->value!=k){
+        if(m->next!=NULL){
+            m=m->next;
+        }else{
+            return 0;
         }
     }
-    else
-    return 1;
+    return m;
 }
-void print(struct list *l)
+
+
+int push_back(struct list* myList, int x) 
 {
-    while (l != NULL)
-    {
-        printf("%d ", l->data);
-        l = l->next;
-    }
-    printf("\n");
-}
-int main()
-{
-    struct list *l, *Temp;
-    int n, a, k, m, t, j , x, z, i;
-    scanf("%d", &n);//1 - считывание количества элементов
-    init(&l);//2 - создание пустого списка
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &a);
-        push_back(l, a);
-        
-    }
-    print(l);//3 - вывод содержимого списка
-    for (i = 0; i < 3; i++)//4 - считывание параметров k
-    {
-        scanf("%d", &k);
-        if (find(l, k) == NULL)
-        printf("0");
-        else
-        printf("1");
-        if (i!=2)
-        printf(" ");
-        else
-        printf("\n");
-    }
-    scanf("%d", &m);//5 - считывание параметра m
-    push_back(l, m);
-    print(l);//6 - вывод содержимого списка
-    scanf("%d", &t);//7 - считывание параметра t
-    push_front(&l, t);
-    print(l);//8 - вывод содержимого списка
-    scanf("%d", &j);//9 - считывание параметров j и x
-    scanf("%d", &x);
-    struct node *el=(struct node *)l;
-    while (j != 1)
-    {
-        el = el->next;
-        j--;
-    }
-    insert_after(el, x);
-    print(l);//10  - вывод содержимого списка
-    scanf("%d", &z);//11  - считывание параметра z
-    if (l->data==z)
-    {
-        Temp=l->next;
-        free(l);
-        l=Temp;
-    }
+    if(is_empty(myList)==1)
+        init (myList,x);
     else
-    remove_node(l, z);
-    print(l);//12  - вывод содержимого списка
-    clean(l);//13 - очистка списка
+    {
+        struct node *m = (struct node*)malloc(sizeof(struct node));
+        m->value=x;
+        m->prev=myList->l;
+        m->next=NULL;
+        myList->l=m;
+        m->prev->next=m;
+    }
     return 0;
 }
 
+
+int push_front(struct list* myList,int x)
+{
+    if(is_empty(myList)==1)
+        init (myList,x);
+    else
+    {
+        struct node *m = (struct node*)malloc(sizeof(struct node));
+        m->value=x;
+        m->prev=NULL;
+        m->next=myList->f;
+        myList->f=m;
+        m->next->prev=m;
+    }
+    return 0;
+}
+
+
+int insert_after (struct list* myList,int num, int data)
+{
+    struct node* m = myList->f;
+    for (int i = 1;i<num;i++)
+    {
+        m=m->next;
+        if(m==NULL)
+            return -1;
+    }
+    struct node *e = malloc(sizeof(node));
+    e->value=data;
+    e->next=m->next;
+    e->prev=m;
+    m->next->prev=e;
+    m->next=e;
+    return 0;
+}
+
+
+int insert_before(struct list* myList,int num, int data)
+{
+    struct node* m = myList->f;
+    for (int i = 1;i<num;i++)
+    {
+        m=m->next;
+        if(m==NULL)
+            return -1;
+    }
+    struct node *e = malloc(sizeof(node));
+    e->value=data;
+    e->prev=m->prev;
+    e->next=m;
+    m->prev=e;
+    e->prev->next=e;
+    return 0;
+}
+
+
+void help_remove(struct list* myList, struct node *m)
+{
+    if(m!=NULL){
+        if (m==myList->f && m==myList->l) {
+            clean(myList);
+            return;
+        }
+        if (m==myList->f)
+        {
+            myList->f=m->next;
+            m->next->prev=NULL;
+        }
+        else if(m==myList->l)
+        {
+            myList->l=m->prev;
+            m->prev->next=NULL;
+        }
+        else if (m!=myList->f && m!=myList->l)
+        {
+            m->next->prev=m->prev;
+            m->prev->next=m->next;
+        }
+        free(m);
+    }
+}
+
+
+int remove_first(struct list* myList, int x)
+{
+    struct node* elem = find(myList, x);
+    if (elem!=NULL){
+        help_remove(myList,elem);
+        return 0;
+    }
+    return -1;
+}
+
+
+struct node* find_invers(struct list* myList, int k)
+{
+    struct node *m=myList->l;
+    while(m->value!=k){
+        if(m->prev!=NULL){
+            m=m->prev;
+        }else{
+            return 0;
+        }
+    }
+    return m;
+}
+
+
+int remove_last(struct list* myList,int g)
+{
+    struct node* elem = find_invers(myList, g);
+    if (elem!=NULL){
+        help_remove(myList,elem);
+        return 0;
+    }
+    return -1;
+}
+
+
+void print(struct list* myList)
+{
+    struct node* m=myList->f;
+    while(m->next!=NULL)
+    {
+        printf("%d ", m->value);
+        m=m->next;
+    }
+    printf("%d\n", m->value);
+}
+
+
+void print_invers(struct list* myList)
+{
+    struct node* m=myList->l;
+    while(m->prev!=NULL)
+    {
+        printf("%d ", m->value);
+        m=m->prev;
+    }
+    printf("%d\n", m->value);
+}
+
+
+int main()
+{
+    int f,a;
+    int k1;
+    int k2;
+    int k3;
+    int m;
+    int t;
+    int j,x;
+    int u,y;
+    int z;
+    int r;
+    struct list* stru40k=(struct list*)malloc(sizeof(struct list));
+    scanf("%d",&f);
+    for (int i=0;i<f;i++)
+    {
+        scanf("%d",&a);
+        push_back(stru40k,a);
+    }
+    print(stru40k);
+    scanf("%d%d%d",&k1,&k2,&k3);
+    if(find(stru40k,k1)!=NULL)
+        printf("1 ");
+    else
+        printf("0 ");
+    if(find(stru40k,k2)!=NULL)
+        printf("1 ");
+    else
+        printf("0 ");
+    if(find(stru40k,k3)!=NULL)
+        printf("1 ");
+    else
+        printf("0 ");
+    printf("\n");
+    scanf("%d",&m);
+    push_back(stru40k,m);
+    print_invers(stru40k);
+    scanf("%d",&t);
+    push_front(stru40k,t);
+    print(stru40k);
+    scanf("%d%d",&j,&x);
+    insert_after(stru40k,j,x);
+    print_invers(stru40k);
+    scanf("%d%d",&u,&y);
+    insert_before(stru40k,u,y);
+    print(stru40k);
+    scanf("%d",&z);
+    remove_first(stru40k,z);
+    print_invers(stru40k);
+    scanf("%d",&r);
+    remove_last(stru40k,r);
+    print(stru40k);
+    clean(stru40k);
+    return 0;
+}
